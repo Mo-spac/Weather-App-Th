3942fc4b427d8a7eb5cc89f514830daa80fd9cd1
Mohammed Samy <mom118580@gmail.com>
2024-12-26 16:35:18 +0200

    create service weather

--- lib/service/weather_service.dart
+++ lib/service/weather_service.dart
@@ -1,1 +1,16 @@
-class WeatherService {}
+import 'package:dio/dio.dart';
+import 'package:weather_app/models/weather_model.dart';
+
+class WeatherService {
+  final Dio dio;
+  final String baseUrl = 'https://api.weatherapi.com/v1';
+
+  WeatherService({required this.dio});
+  Future<WeatherModel> getCurrentWeathr({required String cityName}) async {
+    Response response = await dio.get(
+      '$baseUrl/forecast.json?key=34477d772d904839a9015003242612&q=$cityName&days=10',
+    );
+    WeatherModel weatherModel = WeatherModel.fromJson(response.data);
+    return weatherModel;
+  }
+}
